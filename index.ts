6edce6a1e5427b1ffd9e58846e6792bdf3a94bc8
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-08-12 09:26:49 -0300

    add bubble sort

---
+++ index.ts
@@ -0,0 +1,18 @@
+
+const bubbleSort = (array) => {
+
+    const n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    console.dir(array)
+}
+
+bubbleSort([4,2,3,1])
